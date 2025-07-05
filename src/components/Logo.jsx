4ac4d541fa99c1fa2afa5925a05bbe5c2af1dddc
Ananth <ananthofficemail@gmail.com>
2025-07-05 23:17:14 +0530

    cart svg added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,7 @@
-import cart from "../assets/cart.svg";
 function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <img src={cart} alt="site-logo" className="w-8 h-8" />
+      <img src="cart.svg" alt="site-logo" className="w-8 h-8" />
       <h1 className="text-2xl font-bold">React Shop</h1>
     </div>
   );
