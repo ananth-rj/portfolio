f1b6a036a9c2ec57d143f85a9b56c72d55d41af0
Anantharaj <anand.r@focusresearchlabs.com>
2025-12-03 08:46:30 +0530

    fixed logo error

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <img src="cart.svg" alt="site-logo" className="w-8 h-8" />
+      <img src="/cart.svg" alt="site-logo" className="w-8 h-8" />
       <h1 className="text-2xl font-bold">React Shop</h1>
     </div>
   );
